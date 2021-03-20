68ed70e9c05364b5d8381df635dc3f5b5bd844f4
milja <milja@LAPTOP-42555GQN>
2021-03-20 17:24:48 +0100

    ...

---
+++ OldManSong/src/Program.java
@@ -0,0 +1,39 @@
+import acm.program.ConsoleProgram;
+
+public class Program extends ConsoleProgram {
+	public void run() {
+		println("This is Old Man song");
+	
+	for(int i = 0; i < 10; i++) {
+		println("This old man he played " + (i+1) +  " . ");
+		switch(i+1) {
+		case 1:
+		println("He played knick-knack on my thumb . ");
+		case 2:
+			println("He played knick-knack on my shoe . ");
+		case 3:
+			println("He played knick-knack on my knee . ");
+		case 4:
+			println("He played knick-knack on my door . ");
+		case 5:
+			println("He played knick-knack on my hive . ");
+		case 6:
+			println("He played knick-knack on my sticks . ");
+		case 7:
+			println("He played knick-knack on my heaven . ");
+		case 8:
+			println("He played knick-knack on my pate . ");
+		case 9:
+			println("He played knick-knack on my spine . ");
+		case 10:
+			println("He played knick-knack on my shin . ");
+	
+		}
+		println("Whith a knick-knack, paddy-whack, ");
+		println("Give your dog a bone. ");
+		println("This old man came rolling home. ");
+		
+	}
+
+}
+}
