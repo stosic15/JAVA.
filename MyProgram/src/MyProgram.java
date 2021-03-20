9ed52671a20657c5ed00668c0ee9553418a34655
milja <milja@LAPTOP-42555GQN>
2021-03-20 15:14:04 +0100

    second exercise

---
+++ MyProgram/src/MyProgram.java
@@ -0,0 +1,13 @@
+import acm.program.ConsoleProgram;
+
+public class MyProgram extends ConsoleProgram{
+	public void run() {
+		double n = readDouble("Enter n : ");
+		double n2 = readDouble("Enter n2 : ");
+		double a = (n * n2)/ 2;
+		
+		println("Result is: " +a);
+	}
+
+}
+//Chapter 2 exercises 2
