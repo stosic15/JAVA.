0646b0d68b91ec6b87cd95c81df6bac5c4f563eb
milja <milja@LAPTOP-42555GQN>
2021-03-20 15:48:03 +0100

    ...

---
+++ MiloProgram/src/Program.java
@@ -0,0 +1,15 @@
+import acm.program.ConsoleProgram;
+
+public class Program extends ConsoleProgram {
+	
+	public void run() {
+    	println(" -_- ");
+    	
+        double total = 4+9-2*16+1/3*6-67+8*2-3+26-1/34+3/7+2-5;
+        
+        println(total);
+
+    }
+
+}
+//Chapter 3 exercise 6                 
