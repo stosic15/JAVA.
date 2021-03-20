b0b5d5599ce81cde38ebd7aa3d0d70e2d11c3760
milja <milja@LAPTOP-42555GQN>
2021-03-20 14:46:53 +0100

    first exercises

---
+++ HelloProgram/src/Program.java
@@ -0,0 +1,12 @@
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+
+public class Program extends GraphicsProgram {
+	public void run() {
+		add(new GLabel("I love Java", 100, 75));
+		add(new GLabel("Miljan Stosic", 250, 350));
+		
+	}
+
+}
+//Chapter 2 exercise 1
