ab15d379ce37f49f77dfed8e6597ec68b4dbda91
milja <milja@LAPTOP-42555GQN>
2021-03-20 15:21:28 +0100

    third exercise

--- Add2Integers/src/Program.java
+++ Add2Integers/src/Program.java
@@ -5,10 +5,12 @@ public class Program extends ConsoleProgram {
 		println("This program add two integers");
 		int n1 = readInt("Enter n1: ");
 		int n2 = readInt("Enter n2: ");
+		int n3 = readInt("Enter n3: ");
 		
-		int total = n1 + n2;
+		int total = n1 + n2 + n3;
 		
 		println("Total is " + total + " . ");
 	}
 
 }
+//Chapter 2 exercise 3
