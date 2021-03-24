b8213620d2fe19471d9a18b0c1de32239fbe7062
milja <milja@LAPTOP-42555GQN>
2021-03-24 12:43:11 +0100

    ...

---
+++ AverageList/src/Average.java
@@ -0,0 +1,37 @@
+import acm.program.ConsoleProgram;
+
+public class Average extends ConsoleProgram {
+	
+	public void run() {
+		println("This program adds list average score");
+
+		
+		int total = 0;
+		 int count = 0;
+		int value;
+		while(true) {
+			value = readInt(" Enter value:  ");
+			if (value == STOP) break;
+			total += value;
+			count++;
+			
+			}
+		double avg = (double)total/count;
+		println(avg);
+		
+		
+			 
+			 
+			
+		
+		
+
+
+	}
+		private static final double STOP= -1;
+		
+
+		
+	}
+
+
